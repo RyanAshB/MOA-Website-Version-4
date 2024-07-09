0d693a4d1bb2fb14b8e0c59f742475c46f503e29
Ryan Badaloo <ryanash.b@gmail.com>
2024-07-09 17:03:30 -0500

    Updated Home Page

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def base():
+    return render_template('base.html')
+
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/')
-def base():
-    return render_template('base.html')
+@app.route('/hte-ackee')
+def ackee():
+    return render_template('hte-ackee.html')
 
 
 if __name__ == '__main__':
