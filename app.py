e200599b561b2e2a6d90d639a4934fa2825873f1
Ryan Badaloo <ryanash.b@gmail.com>
2024-07-18 17:11:57 -0500

    18/7/2024 - end

--- app.py
+++ app.py
@@ -1,9 +1,62 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_mail import Mail, Message
-import os
+from flask_mysqldb import MySQL
+import mysql.connector
 
 
 app = Flask(__name__)
+
+db_config = {
+    'user': 'root',
+    'password': 'password123',
+    'host': 'localhost',
+    'database': 'food_trade_data'
+
+}
+
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
+
+@app.route('/dbTest')
+def dbTest():
+    return render_template('dbTest.html')
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    hs_code = request.form['hs_code']
+    ie = request.form['placeholder']
+    year = request.form['year']
+
+    query_variables = []
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    query = "SELECT * FROM food_trade_data WHERE TRUE "
+
+    if hs_code:
+        hs_end = ("AND `HS code`= %s")
+        query = query + hs_end
+        query_variables.append(hs_code)
+
+    if ie:
+        ie_end = ("AND `Trade Type`= %s")
+        query = query + ie_end
+        query_variables.append(ie)
+
+
+    if year:
+        year_end = ("AND `YEAR`= %s")
+        query = query + year_end
+        query_variables.append(year)
+
+    cursor.execute(query, tuple(query_variables))
+    fetchdata = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return jsonify(fetchdata)
+
+
 
 @app.route('/')
 def base():
